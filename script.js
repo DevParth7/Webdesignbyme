24990a7dfa8a6b6d828efe4589bf1ab37eab43bc
Dev Parth <106392401+DevParth7@users.noreply.github.com>
2022-06-03 22:19:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+// (function (doc) { var scriptElm= doc.scripts[doc.scripts.lenght -1];
+//     var warn=['[ionicons'] 
+
+//     warn.push('to improve performance it is recommended to set the differential scripts in the heads as follow:')
+//     var parts = scriptElm.src.split('/');
+//     parts.pop();
+//     parts.push('ionicons');
+//     var url= parts.join('/')
+
+//     var scriptElm= doc.createElement('script');
+//     scriptElm.setAttribute('type, module');
+//     scriptElm.src= url+'ionicons.esm.js',
+//     warn.push(scriptElm.outerHTML);
+//     scriptElm.setAttribute('data-stencil-namespace', 'ionicons');
+//     doc.head.appendChild(scriptElm);
+//     scriptElm=doc.createElement('scripts');
+//     scriptElm.setAttribute('nomodule','');
+//     scriptElm.src=url+'/ionicons.js';
+//     warn.push(scriptElm.outerHTML);
+//     scriptElm.setAttribute('data-stencil-namespace', 'ionicons');
+//     doc.head.appendChild(scriptElm)
+
+//     console.warn(warn.join('\n'));
+
+    
+// })(document);
